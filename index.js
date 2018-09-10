05f308d7b3162d7d61e7ccd3c1e3bac959521355
unknown <cjgeerts@gmail.com>
2018-09-10 12:23:21 -0400

    stupid/fun updates

--- index.js
+++ index.js
@@ -4,21 +4,50 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const emojis = require('./emoji.json');
+
 // console.log(settings.token);
 // console.log(settings.prefix);
 
 client.on('ready', () => {
     client.generateInvite(["ADMINISTRATOR"]).then(link =>{
         console.log(link);
+        console.log();
     });
 });
 
 client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === 'ping') {
-      // Send "pong" to the same channel
-      message.channel.send('pong');
+    //Every time the server is messaged (no matter the channel) these events occur
+    let messageCheck=message.content.toLowerCase();
+
+    if (messageCheck.includes('like')) {
+      message.channel.send('👍');
     }
+
+    if (messageCheck.includes('bad bot')) {
+        message.channel.send('👿');
+    }
+
+    if (messageCheck.includes('good bot')) {
+        message.channel.send('👼');
+    }
+
+    if (messageCheck.includes('sad')) {
+        message.channel.send('😭');
+    }
+
+    if (messageCheck.includes('thanks')||messageCheck.includes('thnx')) {
+        message.channel.send('😎');
+    }
+
+    if (messageCheck.includes('ewok')&&messageCheck.includes('daily')&&messageCheck.includes('shitpost')){
+        message.channel.send('https://imgur.com/random');
+    }
+
+    if(Math.floor(Math.random()*500)==0){
+        message.channel.send(emojis[Math.floor(Math.random()*emojis.length)].emoji);
+    }
+
   });
 
 client.login(settings.token);
