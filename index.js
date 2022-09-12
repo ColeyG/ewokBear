3bb542ecbc965712bb42d2d333b43ac075fb2eae
ColeyG <cjgeerts@gmail.com>
2022-09-12 11:52:39 -0400

    Removing extraneous bits

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const settings = require('./config.json');
 
 // scripts imported
 const emojis = require('./emoji.json');
-const discipline = require('./scripts/Discipline');
-const miscMessages = require('./scripts/MiscMessages');
 const helpers = require('./scripts/Helpers');
-const funmsg = require('./scripts/Responses.json');
 
 client.on('ready', () => {
   client.generateInvite(['ADMINISTRATOR']).then((link) => {
@@ -24,10 +21,14 @@ client.on('error', (error) => {
 client.on('message', (message) => {
   // Every time the server is messaged (no matter the channel) these events occur
   const messageCheck = message.content.toLowerCase();
-  const checks = Object.keys(funmsg);
 
-  // General message checks
-  miscMessages.miscMessages(messageCheck, checks, message);
+  if (messageCheck.includes('bad bot')) {
+    message.channel.send('👿');
+  }
+
+  if (messageCheck.includes('good bot')) {
+    message.channel.send('👼');
+  }
 
   // If a message has attachment(s)
   const attachments = [...message.attachments.keys()];
@@ -38,19 +39,6 @@ client.on('message', (message) => {
       }
     });
   }
-});
-
-client.on('messageDelete', (message) => {
-  console.log(`${message.member.user.tag}'s post was deleted`);
-  const name = message.member.user.tag;
-  // message.channel.send(name + "'s post was deleted!... I saw that ...");
-});
-
-client.on('guildMemberAdd', (member) => {
-  // when a user is added, these events fire
-  const channel = member.guild.channels.find((ch) => ch.name === 'member-log');
-  if (!channel) return;
-  channel.send(`Get out, ${member}`);
 });
 
 client.login(settings.token);
